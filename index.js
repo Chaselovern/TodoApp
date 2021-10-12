e89d750deaabbc6164f89b39f84fe915039980ef
Chase Lovern <Chaselovern@gmail.com>
2021-10-11 19:58:50 -0600

    added heroku and mongodb connections

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const uri = process.env.MONGODB_URI;
 const port = 8000;
 
 const todoRoutes = require("./routes/todo");
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoapp", {
+mongoose.connect("mongodb+srv://chaseLovern:GGLO0xRaXSMDkgvz@cluster0.edve9.mongodb.net/todoapp?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useCreateIndex: true,
